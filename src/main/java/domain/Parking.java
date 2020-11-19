78ab980d21b121778fa15bbf25a4dd0911f629e8
JuHyun419 <zzang9ha@naver.com>
2020-11-19 22:03:49 +0900

    add Parking

---
+++ src/main/java/domain/Parking.java
@@ -0,0 +1,36 @@
+package domain;
+
+import vehicle.Vehicle;
+
+import java.util.List;
+
+public class Parking {
+
+    private int vehicleCount;
+    private List<Vehicle> vehicles;
+
+
+    public int getVehicleCount() {
+        return vehicleCount;
+    }
+
+    public void setVehicleCount(int vehicleCount) {
+        this.vehicleCount = vehicleCount;
+    }
+
+    public List<Vehicle> getVehicles() {
+        return vehicles;
+    }
+
+    public void setVehicles(List<Vehicle> vehicles) {
+        this.vehicles = vehicles;
+    }
+
+    public Parking() {
+    }
+
+    public Parking(int vehicleCount) {
+        this.vehicleCount = vehicleCount;
+    }
+
+}
