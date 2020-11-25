093ee598920d8643a1d9a16ced9844757f20d27e
JuHyun419 <zzang9ha@naver.com>
2020-11-25 23:22:09 +0900

    add BusType Enum

--- src/main/java/bus/BusType.java
+++ src/main/java/bus/BusType.java
@@ -2,6 +2,24 @@ package bus;
 
 public enum BusType {
 
-    LARGEBUS, MEDIUMBUS, MINIBUS,
+    LARGE_BUS(5000, 3000),
+    MEDIUM_BUS(3000, 2000),
+    MINI_BUS(2000, 1000);
+
+    private final int defaultPrice;
+    private final int halfHourlyPrice;
+
+    private BusType(int defaultPrice, int halfHourlyPrice) {
+        this.defaultPrice = defaultPrice;
+        this.halfHourlyPrice = halfHourlyPrice;
+    }
+
+    public int getDefaultPrice() {
+        return defaultPrice;
+    }
+
+    public int getHalfHourlyPrice() {
+        return halfHourlyPrice;
+    }
 
 }
