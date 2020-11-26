4d36ae874104d53f387b12b3fb36203912e2e676
JuHyun419 <zzang9ha@naver.com>
2020-11-27 00:08:39 +0900

    add DateUtils TestCode

--- src/test/java/util/DateUtilsTest.java
+++ src/test/java/util/DateUtilsTest.java
@@ -21,4 +21,40 @@ public class DateUtilsTest {
         // Then
         assertThat(newTime).isEqualTo("2020/11/24 23:30");
     }
+
+    @DisplayName("주차 시간 테스트")
+    @Test
+    public void 주차_시간_테스트() {
+
+        // Given
+        String entranceTime1 = "202011251200";
+        String exitTime1 = "202011251330";
+
+        String entranceTime0 = "202011251300";
+        String exitTime0 = "202011251330";
+
+        // When
+        int diffHours1 = DateUtils.getDiffHours(entranceTime1, exitTime1);
+        int diffHours2 = DateUtils.getDiffHours(entranceTime0, exitTime0);
+
+        // Then
+        assertThat(diffHours1).isEqualTo(1);
+        assertThat(diffHours2).isEqualTo(0);
+    }
+    
+    @DisplayName("주차 분 테스트")
+    @Test
+    public void 주차_분_테스트() {
+
+        // Given
+        String entranceTime = "202011251200";
+        String exitTime = "202011251330";
+
+        // When
+        int diffMinutes = DateUtils.getDiffMinutes(entranceTime, exitTime);
+
+        // Then
+        assertThat(diffMinutes).isEqualTo(30);
+
+    }
 }
