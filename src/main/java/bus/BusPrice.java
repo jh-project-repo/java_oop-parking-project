c8ad1d366785694aca64f2085fe3434dd1d5e848
JuHyun419 <zzang9ha@naver.com>
2020-12-04 20:06:35 +0900

    필요없는 주석 제거

--- src/main/java/bus/BusPrice.java
+++ src/main/java/bus/BusPrice.java
@@ -8,24 +8,7 @@ public class BusPrice {
     public static int getPrice(BusType busType, final int hour, final int minute) {
         int defaultPrice = busType.getDefaultPrice();
         int halfHourlyPrice = busType.getHalfHourlyPrice();
-        System.out.println("defaultPrice: " + defaultPrice);
-        System.out.println("halfHourlyPrice: " + halfHourlyPrice);
         return getPrice(hour, minute, defaultPrice, halfHourlyPrice);
-
-        // 딱히 switch로 안나누고 바로 getPrice 함수 호출해도 될듯..? 왜냐면 인자로 BusType를 받으니까?
-//        switch (busType) {
-//            case LARGE_BUS:
-//                price = getPrice(hour, minute, defaultPrice, halfHourlyPrice);
-//                break;
-//            case MEDIUM_BUS:
-//                price = getPrice(hour, minute, defaultPrice, halfHourlyPrice);
-//                break;
-//            case MINI_BUS:
-//                price = getPrice(hour, minute, defaultPrice, halfHourlyPrice);
-//                break;
-//            default:
-//                throw new IllegalStateException(busType + "에 해당하는 버스 종류가 없습니다.");
-//        }
     }
 
     private static int getPrice(final int hour, final int minute, final int defaultPrice, final int halfHourlyPrice) {
