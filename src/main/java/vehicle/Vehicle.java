fb3dadcb3c5fb7f5e5a757ed8caf957265978866
JuHyun419 <zzang9ha@naver.com>
2020-12-04 20:08:46 +0900

    Vehicle 인터페이스 제거 & Vehicles 추상클래스 추가

--- src/main/java/vehicle/Vehicle.java
+++
@@ -1,5 +0,0 @@
-package vehicle;
-
-public interface Vehicle {
-    int getPrice();
-}
---
+++ src/main/java/vehicle/Vehicles.java
@@ -0,0 +1,19 @@
+package vehicle;
+
+public abstract class Vehicles {
+
+    protected int vehicleNumber;
+
+    protected String entranceTime;
+
+    // 처음 설계할때부터 주차요금을 구할 때, 시간 + 분 이 아닌 총 분으로 메서드를 만들면...
+    // 매개변수도 1개로 감소되고 더 좋을듯...?
+    public abstract int getPrice(int hour, int minute);
+
+    public abstract int getVehicleNumber();
+
+    public abstract String getEntranceTime();
+
+    public abstract String toString();
+
+}
