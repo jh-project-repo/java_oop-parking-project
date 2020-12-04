122632544ca340b9193525b303688b99d4f5961d
JuHyun419 <zzang9ha@naver.com>
2020-12-04 23:18:36 +0900

    Menu 입력에 대한 상수 클래스 추가  ==> 가독성 ↑

---
+++ src/main/java/util/Menu.java
@@ -0,0 +1,20 @@
+package util;
+
+/**
+ * 메뉴 Input에 대한 상수 클래스 (코드리뷰)
+ *  ==> menu == 1 으로 설정하는 것 보다, 
+ *  상수로 각각 설정한 후 가독성이 좋게(확실하게) 표현
+ */
+public class Menu {
+
+    public static final int INPUT_CAR = 1;
+
+    public static final int EXIT_CAR = 2;
+
+    public static final int PRINT_PARKING_CAR = 3;
+
+    public static final int PRINT_ALL_INCOME = 4;
+
+    public static final int EXIT = 5;
+
+}
